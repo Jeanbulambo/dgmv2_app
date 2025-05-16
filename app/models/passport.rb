c2801115608bf25609c8cb3e5eea1d21b61fe673
Jeanbulambo <jeanbulambo4@gmail.com>
2025-05-16 21:17:17 +0000

    getting some changes related to design and configured export functionality

--- app/models/passport.rb
+++ app/models/passport.rb
@@ -1,2 +1,21 @@
+# app/models/passport.rb
+
+require "csv"
+
 class Passport < ApplicationRecord
+  # Validations de base
+  validates :prenom, :nom, :numero_passport, presence: true
+
+  # Export CSV
+  def self.to_csv
+    attributes = column_names
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.find_each do |passport|
+        csv << attributes.map { |attr| passport.send(attr) }
+      end
+    end
+  end
 end
